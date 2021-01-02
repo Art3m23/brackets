b2447395df47ff16ae1282e1df40ab9bc94510ec
Art3m23 <76261475+Art3m23@users.noreply.github.com>
2021-01-02 17:16:20 +0300

    feat: add functions code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  while(str !== ''){
+        let j = 0
+        for (let i=0;i<bracketsConfig.length;i++) {
+            let string = bracketsConfig[i][0] + bracketsConfig[i][1]
+            if (str.includes(string)){
+                str = str.replace(string, '')
+                j++
+            }
+        }
+        if (j === 0) break
+    }
+    if (str === '') 
+		return true
+    return false
 }
